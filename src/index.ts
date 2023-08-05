6d85730a0eba082049f6850ba109fa469af98b0b
wobsoriano <sorianorobertc@gmail.com>
2023-08-04 20:18:34 -0700

    feat: add support for compose imports

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,8 @@ export default function lightningcssPlugin(): import('bun').BunPlugin {
         }
       })
 
-      const escape = (string: string) => JSON.stringify(string).slice(1, -1)
+      const quote = JSON.stringify
+      const escape = (string: string) => quote(string).slice(1, -1)
 
       onLoad({ filter: /\.module\.css$/ }, async ({ path }) => {
         const rawCssBuffer = await fs.readFile(path)
@@ -38,48 +39,57 @@ export default function lightningcssPlugin(): import('bun').BunPlugin {
           filename: path,
           code: rawCssBuffer,
           cssModules: {
-              pattern: `[hash]_[local]`
+            pattern: `[hash]_[local]`
           },
           projectRoot: join(process.cwd(), config.outdir || 'dist'),
-        })
-
+        });
+        
         let contents = '';
 
-        contents += `import { injectStyle } from '__style_helper__'\n`;
-        contents += `injectStyle(${JSON.stringify(code.toString())})\n`
-        contents += `export default {`;
+        const dependencies = new Map<string, string>()
 
-        for (const [cssClassReadableName, cssClassExport] of Object.entries(exports)) {
-            let compiledCssClasses = `"${escape(cssClassExport.name)}`
-
-            if (cssClassExport.composes) {
-                for (const composition of cssClassExport.composes) {
-                    switch (composition.type) {
-                        case "local":
-                            compiledCssClasses += " " + escape(composition.name)
-                            break;
-                    
-                        case "global":
-                            compiledCssClasses += " " + escape(composition.name)
-                            break;
-
-                       // TODO: dependency import
-                    }
-                }
+        const importDependency = (path: string) => {
+            if (dependencies.has(path)) {
+                return dependencies.get(path)
             }
-
-            compiledCssClasses += `"`
-
-            contents += `${JSON.stringify(cssClassReadableName)}:${compiledCssClasses},`
+            const dependenciesName = `dependency_${dependencies.size}`
+            // prepend dependency to to the contents
+            contents = `import ${dependenciesName} from ${quote(path)}\n` + contents;
+            dependencies.set(path, dependenciesName)
+            return dependenciesName;
         }
 
-        contents += "}"
-
+        contents += `import { injectStyle } from '__style_helper__'\n`;
+        contents += `injectStyle(${quote(code.toString())})\n`;
+        contents += `export default {`;
+        
+        for (const [cssClassReadableName, cssClassExport] of Object.entries(exports)) {
+          let compiledCssClasses = `"${escape(cssClassExport.name)}`;
+        
+          if (cssClassExport.composes) {
+            for (const composition of cssClassExport.composes) {
+              switch (composition.type) {
+                case "local":
+                case "global":
+                  compiledCssClasses += " " + escape(composition.name);
+                  break;
+                case "dependency":
+                  compiledCssClasses += ` " + ${importDependency(composition.specifier)}[${quote(composition.name)}] + "`
+                  break;
+              }
+            }
+          }
+        
+          compiledCssClasses += `"`;
+        
+          contents += `${JSON.stringify(cssClassReadableName)}:${compiledCssClasses},`;
+        }
+        
+        contents += "}";
+        
         // https://github.com/evanw/esbuild/issues/2943#issuecomment-1439755408
         const emptyishSourceMap = "data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ==";
-        contents += `\n//# sourceMappingURL=${emptyishSourceMap}`
-
-        console.log(contents);
+        contents += `\n//# sourceMappingURL=${emptyishSourceMap}`;
 
         return {
           contents,
