ec76fbdfbedcc2aafe677d2d6e684afae502b982
wobsoriano <sorianorobertc@gmail.com>
2023-08-04 19:42:03 -0700

    update build script

--- build.mjs
+++ build.mjs
@@ -4,5 +4,7 @@ await Bun.build({
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
   minify: true,
-  plugins: [dts()]
+  plugins: [dts()],
+  target: 'bun',
+  external: ['lightningcss'],
 })
